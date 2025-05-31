c6b59efc048dd03700d723924cfe2a2686ec3ce2
GADHIYA PRIT <pritgadhiya37@gmail.com>
2025-05-31 17:42:34 +0000

    test

---
+++ basic/datatype.js
@@ -0,0 +1,15 @@
+// alert (3+3) we are using nodejs,not browser console
+ // code readibility 
+  let name="prit" //string
+  let age=18   //numbers ,big int
+  let islogin=true //boolean
+  // null = standalone value     empty
+  // undefined =does not give value           let state;
+  // symbols  = unique
+  console.log(typeof"prit");    //string
+  console.log(typeof(name));  //string
+  console.log(typeof age);       //number
+  console.log(typeof islogin);    // boolen
+  console.log(typeof null);       // object
+  console.log(typeof undefined);  // undefined
+  
